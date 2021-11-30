9dbc241268c349d2246e418458976bccf45613f8
raj-reddy1 <93119641+raj-reddy1@users.noreply.github.com>
2021-11-30 22:22:09 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_1.java
@@ -0,0 +1,15 @@
+package testng_activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4_1 {
+	@Test
+    public void testCaseOne() {
+        System.out.println("I'm in test case one from Activity4_1 Class");
+    }
+
+    @Test
+    public void testCaseTwo() {
+        System.out.println("I'm in test case two from Activity4_1 Class");
+    }
+}
